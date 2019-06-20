273b25e26f120ea1e790f00ca0f9238e9e8b5bea
mahmoud <mahmoud.elebiary@yahoo.com>
2019-06-21 00:57:39 +0200

    Fix error in retrofit

--- app/src/main/java/com/example/yalladealz/ApiCall/GetUser.java
+++ app/src/main/java/com/example/yalladealz/ApiCall/GetUser.java
@@ -4,10 +4,10 @@ import com.example.yalladealz.Model.LoginModel;
 import com.example.yalladealz.Util.Constant;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
 import retrofit2.http.POST;
-import retrofit2.http.Part;
 
 public interface GetUser {
     @POST(Constant.LOGIN_URL)
-    Call<LoginModel> Login(@Part("email")String email,@Part("password")String password);
+    Call<LoginModel> Login(@Field("email")String email, @Field("password")String password);
 }
