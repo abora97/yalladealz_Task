04c076f04958ccd68efe4cfa49126d353cc9a88a
mahmoud <mahmoud.elebiary@yahoo.com>
2019-06-21 01:02:36 +0200

    fix Encoded error

--- app/src/main/java/com/example/yalladealz/ApiCall/GetUser.java
+++ app/src/main/java/com/example/yalladealz/ApiCall/GetUser.java
@@ -5,9 +5,11 @@ import com.example.yalladealz.Util.Constant;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface GetUser {
     @POST(Constant.LOGIN_URL)
+    @FormUrlEncoded
     Call<LoginModel> Login(@Field("email")String email, @Field("password")String password);
 }
